df9a3b1aae8f25ad52e0bed451310bded68354b3
J1u2a3n21 <85364412+J1u2a3n21@users.noreply.github.com>
2021-06-04 13:43:57 -0400

    Add files via upload

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,123 @@
+﻿using McNutsAPI.Exceptions;
+using McNutsAPI.Models;
+using McNutsAPI.Services;
+using MCNutsAPI.Exceptions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace McNutsAPI.Controllers
+{
+    [Route("api/peanuts/{peanutId:long}/[controller]")]
+    public class ClientsController : ControllerBase
+    {
+        private IClientService _clientService;
+
+        public ClientsController(IClientService clientsService)
+        {
+            _clientService = clientsService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ClientModel>>> GetClientsAsync(long peanutId)
+        {
+            try
+            {
+                var clients = await _clientService.GetClientsAsync(peanutId);
+                return Ok(clients);
+            }
+            catch (InvalidOperationClientException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Algo Inesperado Paso. ");
+            }
+
+        }
+
+        [HttpGet("{ci:long}")]
+        public async Task<IActionResult> GetClientAsync(long peanutId, long ci)
+        {
+            try
+            {
+                var client = await _clientService.GetClientAsync(peanutId, ci);
+                return Ok(client);
+            }
+            catch (NotFoundClientException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Algo Inesperado Paso. ");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ClientModel>> CreateClientAsync(long peanutId, [FromBody] ClientModel newClient)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                var createdClient = await _clientService.CreateClientAsync(peanutId, newClient);
+                return Created($"/api/peanuts/{peanutId}/clients/{createdClient.Ci}", createdClient);
+
+            }
+            catch (ClientExistException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Algo Inesperado Paso. ");
+            }
+        }
+
+        [HttpDelete("{ci:long}")]
+        public async Task<ActionResult<bool>> DeleteClientAsync(long peanutId, long ci)
+        {
+            try
+            {
+                var result = await _clientService.DeleteClientAsync(peanutId, ci);
+                return Ok(result);
+            }
+            catch (NotFoundClientException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Algo Inesperado Paso. ");
+            }
+        }
+
+        [HttpPut("{ci:long}")]
+        public async Task<ActionResult<ClientModel>> UpdateClientAsync(long peanutId, long ci, [FromBody] ClientModel updateClient)
+        {
+            try
+            {
+                var clientUpdate = await _clientService.UpdateClientAsync(peanutId, ci, updateClient);
+                return Ok(clientUpdate);
+            }
+            catch (NotFoundClientException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Algo Inesperado Paso. ");
+            }
+        }
+
+        //Hacer un end point restablecer stock 
+
+    }
+}
---
+++ Controllers/PeanutController.cs
@@ -0,0 +1,148 @@
+﻿using McNutsAPI.Exceptions;
+using McNutsAPI.Models;
+using McNutsAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace McNutsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class PeanutsController : Controller
+    {
+
+        private IPeanutService _peanutsService;
+        public PeanutsController(IPeanutService peanutsService)
+        {
+            _peanutsService = peanutsService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PeanutModel>>> GetPeanutsAsync(string orderBy = "Id")
+        {
+            try
+            {
+                var peanuts = await _peanutsService.GetPeanutsAsync(orderBy);
+                return Ok(peanuts);
+            }
+            catch (InvalidOperationPeanutException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Algo Inesperado Paso. ");
+            }
+
+        }
+        [HttpGet("{peanutId:long}")]
+        public async Task<ActionResult<PeanutModel>> GetPeanutAsync(long peanutId)
+        {
+            try
+            {
+                var peanut = await _peanutsService.GetPeanutAsync(peanutId);
+                return Ok(peanut);
+
+            }
+            catch (NotFoundPeanutException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Algo Inesperado Paso. ");
+            }
+
+        }
+        [HttpPost]
+        public async Task<ActionResult<PeanutModel>> CreatePeanutAsync([FromBody] PeanutModel newPeanut)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                var peanut = await _peanutsService.CreatePeanutAsync(newPeanut);
+                return Created($"/api/peanuts/{peanut.Id}", peanut);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Algo Inesperado Paso. ");
+            }
+
+        }
+        [HttpDelete("{peanutId:long}")]
+        public async Task<ActionResult<bool>> DeletePeanutAsync(long peanutId)
+        {
+            try
+            {
+                var result = await _peanutsService.DeletePeanutAsync(peanutId);
+                return Ok(result);
+
+            }
+            catch (NotFoundPeanutException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Algo Inesperado Paso. ");
+            }
+        }
+
+        [HttpPut("{peanutId:long}")]
+        public async Task<ActionResult<PeanutModel>> UpdatePeanutAsync(long peanutId, [FromBody] PeanutModel updatePeanut)
+        {
+            try
+            {
+                var peanut = await _peanutsService.UpdatePeanutAsync(peanutId, updatePeanut);
+                return Ok(peanut);
+            }
+            catch (NotFoundPeanutException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Algo Inesperado Paso. ");
+            }
+        }
+
+        [HttpPatch("{peanutId:long}")]
+        public async Task<ActionResult<PeanutModel>> RestoreProduction(long peanutId)
+        {
+            try
+            {
+                var peanut = await _peanutsService.RestoreProductionAsync(peanutId);
+                return Ok(peanut);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Algo Inesperado Paso. ");
+            }
+
+        }
+
+        [HttpPut("{peanutId:long}/updatestock")]
+        public async Task<ActionResult<PeanutModel>> UpdateStockAsync(long peanutId, [FromBody] long amount)
+        {
+            try
+            {
+                var peanut = await _peanutsService.UpdateStockAsync(peanutId, amount);
+                return Ok(peanut);
+            }
+            catch (InsufficientAmountPeanuts ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Algo Inesperado Paso. ");
+            }
+        }
+
+    }
+}
